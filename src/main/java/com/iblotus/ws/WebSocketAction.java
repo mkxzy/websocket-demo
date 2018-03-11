13a07fdc5f582343f6fc667725dc188572d92a64
Michael <makina67@sina.com>
2018-03-12 03:18:07 +0800

    测试

--- src/main/java/com/iblotus/ws/WebSocketAction.java
+++ src/main/java/com/iblotus/ws/WebSocketAction.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
@@ -17,28 +18,15 @@ public class WebSocketAction {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @MessageMapping("/sendTest")
-    @SendTo("/topic/subscribeTest")
+    @MessageMapping("/xxx")
+    @SendTo("/topic/a/fuck")
     public ServerMessage sendDemo(ClientMessage message) {
         logger.info("接收到了信息" + message.getName());
         return new ServerMessage("你发送的消息为:" + message.getName());
     }
 
-    @SubscribeMapping("/subscribeTest")
+    @SubscribeMapping("/xxx")
     public ServerMessage sub() {
-        logger.info("XXX用户订阅了我。。。");
-        return new ServerMessage("感谢你订阅了我。。。");
-    }
-
-    @MessageMapping("/sendTest2")
-    @SendTo("/queue/xxx")
-    public ServerMessage sendDemoQueue(ClientMessage message) {
-        logger.info("接收到了信息" + message.getName());
-        return new ServerMessage("你发送的消息为:" + message.getName());
-    }
-
-    @SubscribeMapping("/subscribeTest2")
-    public ServerMessage subQueue() {
         logger.info("XXX用户订阅了我。。。");
         return new ServerMessage("感谢你订阅了我。。。");
     }
